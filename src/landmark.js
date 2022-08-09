9ca9167f23ee9007f1a7b7d768880f7b71a0297a
Endy135 <endinoungang@gmail.com>
2022-08-09 14:56:57 +0100

    landmark

--- src/landmark.js
+++ src/landmark.js
@@ -1,53 +1,167 @@
 class Landmark{
-    constructor(x_box, y_box, width, height, prop = {}){
+    constructor(x_box, y_box, width, height, prop = {}, display_x_graduations = false, display_y_graduations = false){
         this.x0 = x_box;
         this.y0 = y_box + height;
         this.width = width;
         this.height = height;
         this.x_axis = "";
         this.y_axis = "";
-        this.y_pas_min = (this.height == 1) ? this.height/2 : 1;
-        this.x_pas_min = (typeof prop.label[0] == "string") ? this.width / prop.label.length : (this.width <= 1) ? this.width/2 : 1;
+        this.display_x_graduations = display_x_graduations;
+        this.display_y_graduations = display_y_graduations;
+        if(prop.x ){ 
+            if(typeof prop[prop.x][0] == "string")
+                this.x_pas_min = this.width / prop[prop.x].length;
+            else
+                this.x_pas_min = 10;
+        }
+        else{
+            var key;
+            Object.keys(prop).map((v, index) =>{
+                if(index == 0)
+                    key = v; 
+            });
+            if(typeof prop[key][0] == "string")
+                this.x_pas_min = this.width / prop[key].length;
+            else
+                this.x_pas_min = 1;
+        }
+        if(prop.y ){ 
+            if(typeof prop[prop.y][0] == "string")
+                this.y_pas_min = this.height / prop[prop.y].length;
+            else
+                this.y_pas_min = 1;
+        }
+        else{
+            var key;
+            Object.keys(prop).map((v, index) =>{
+                if(index == 1)
+                    key = v; 
+            });
+            if(typeof prop[key][0] == "string")
+                this.y_pas_min = this.height / prop[key].length;
+            else
+                this.y_pas_min = 1;
+        }
+
+    
         this.y_pas = [];
         this.x_pas = [];
+        this.create_y_pas(prop);
+        this.create_x_pas(prop);
         this.create_axis();
-        this.create_y_pas();
-        this.create_x_pas();
     }
 
     create_axis(){
-        this.x_axis = aya.Line(this.x0, this.y0, this.x0 + this.width, this.y0);
-        this.y_axis = aya.Line(this.x0, this.y0, this.x0, this.y0 - this.height);
-    }
-
-    create_y_pas(){
-        var i, y;
-        var tmp = this.height / 2;
-        while(tmp > this.y_pas_min ){ 
-            tmp = tmp / 2;
-        }
-        console.log(tmp);
-        tmp = tmp*2;
-        y = this.y0;
-        console.log(tmp);
-        for(i = 0; i < (this.height/tmp); i++){
-            this.y_pas[i] = y;
-            y += tmp;
-        }
-    }
-
-    create_x_pas(){
-        var i;
-        var tmp = this.width/2;
-        while(tmp > this.x_pas_min)
-            tmp = tmp/2;
-            console.log(tmp);
-            tmp = tmp*2;
-            console.log(tmp);
-            for(i = 0; i < (this.width/tmp); i++){
-                this.x_pas[i] = this.x0 + i*tmp;
+        this.x_axis = aya.Polyline([this.x0, this.y0, this.x0 + this.width, this.y0]);
+        this.y_axis = aya.Polyline([this.x0, this.y0, this.x0, this.y0 - this.height]);
+        if(this.display_x_graduations){
+            for(var i = 0; i < this.x_pas.length; i++){
+                var pl = aya.Polyline([this.x_pas[i], this.y0 - 3, this.x_pas[i], this.y0 + 3]);
+                //var pl = aya.Polyline([0, 0, 0, 0]);
+                this.x_axis.addChild(pl, (p,c) =>{}, null, true);
             }
+        }
+        if(this.display_y_graduations){
+            for(var i = 0; i < this.y_pas.length; i++){
+                var pc = aya.Polyline([this.x0 -3, this.y_pas[i], this.x0 + 3, this.y_pas[i]]);
+                this.y_axis.addChild(pc, (p,c) =>{}, null, true);
+            }
+        }
+        this.x_axis.draw();
+        this.y_axis.draw();
     }
+
+    create_y_pas(prop){
+        var i, y, tmp, key;
+        if(prop.y){
+            if(typeof prop[prop.y][0] == "string"){
+                for( i = 0; i < this.height/prop[prop.y].length; i++){
+                    this.y_pas[i] = this.y0 - i*this.y_pas_min;
+                }
+            }
+            else{
+                tmp = this.height / 2;
+                while(tmp > this.y_pas_min ){ 
+                    tmp = tmp / 2;
+                }
+                console.log(tmp);
+                tmp = tmp*2;
+                y = this.y0;
+                console.log(tmp);
+                for(i = 0; i < (this.height/tmp); i++){
+                    this.y_pas[i] = y;
+                    y -= tmp;
+                }
+            }
+        }
+        else{
+            Object.keys(prop).map((v, index) =>{
+                if(index == 1)
+                    key = v;
+            });
+            if(typeof prop[key][0] == "string")
+                for (i = 0; i < this.height/this.y_pas_min; i++)
+                    this.y_pas[i] = this.y0 - i*this.y_pas_min;
+            else{
+                tmp = this.height/2;
+                while(tmp > this.y_pas_min)
+                    tmp = tmp/2;
+                console.log(tmp);
+                tmp = tmp*2;
+                console.log(tmp);
+                for(i = 0; i < (this.height/tmp); i++){
+                    this.y_pas[i] = this.y0 - i*tmp;
+                }
+            }
+        }
+        
+        
+    }
+
+    create_x_pas(prop){
+        var i, tmp, key;
+        if(prop.x ){
+            if(typeof prop[prop.x][0] == "string")
+                for (i = 0; i < this.width/this.x_pas_min; i++)
+                    this.x_pas[i] = this.x0 + i*this.x_pas_min;
+            else{
+                tmp = this.width/2;
+                while(tmp > this.x_pas_min)
+                    tmp = tmp/2;
+                console.log(tmp);
+                tmp = tmp*2;
+                console.log(tmp);
+                for(i = 0; i < (this.width/tmp); i++){
+                    this.x_pas[i] = this.x0 + i*tmp;
+                }
+            }
+            
+        }
+        else{
+            Object.keys(prop).map((v, index) =>{
+                if(index == 0)
+                    key = v;
+            });
+            if(typeof prop[key][0] == "string")
+                for (i = 0; i < this.width/this.x_pas_min; i++)
+                    this.x_pas[i] = this.x0 + i*this.x_pas_min;
+            else{
+                tmp = this.width/2;
+                while(tmp > this.x_pas_min)
+                    tmp = tmp/2;
+                console.log(tmp);
+                tmp = tmp*2;
+                console.log(tmp);
+                for(i = 0; i < (this.width/tmp); i++){
+                    this.x_pas[i] = this.x0 + i*tmp;
+                }
+            }
+           
+        }
+    }
+   
+
+   
 
 
 }
--- test/landmark.test.js
+++ test/landmark.test.js
@@ -1,8 +1,9 @@
 QUnit.module("Landmark");
-/*QUnit.test("creation of landmark with box's diagram parameters", (assert) =>{
+QUnit.test("creation of landmark with box's diagram parameters", (assert) =>{
     var prop = {
-        data : [12, 234, 17, 29],
-        label: ["un", "deux", "trois", "quatre"]
+        age : [12, 234, 17, 29],
+        sexe: ["un", "deux", "trois", "quatre"],
+        nom: ['David', 'michel', 'Sarah', 'Paule']
     }
     var ld = new Landmark(10, 10, 1000, 1000, prop);
     assert.equal(ld.x0, 10, "the x_origin must be defined");
@@ -13,93 +14,90 @@ QUnit.module("Landmark");
 
 QUnit.test("check if the absissa line and ordinate line are well draw", (assert) =>{
     var prop = {
-        data : [12, 234, 17, 29],
-        label: ["un", "deux", "trois", "quatre"]
+        age : [12, 234, 17, 29],
+        sexe: ["un", "deux", "trois", "quatre"],
+        nom: ['David', 'michel', 'Sarah', 'Paule']
     }
     var ld = new Landmark(10, 9000, 1000, 1000, prop);
 
-    assert.equal(ld.x_axis.type, "line", "type must be line");
+    assert.equal(ld.x_axis.type, "polyline", "type must be line");
     assert.equal(ld.x_axis.x, ld.x0, "x_abscissa must be ten");
     assert.equal(ld.x_axis.y, ld.y0, "y_abscissa must be ten");
     assert.equal(ld.x_axis.dest_x, ld.x0 + ld.width, "dest_x must be a one thousand and ten");
     assert.equal(ld.x_axis.dest_y, ld.y0, "dest_y absissa must be ten");
 
-    assert.equal(ld.y_axis.type, "line", "type must be line");
+    assert.equal(ld.y_axis.type, "polyline", "type must be line");
     assert.equal(ld.y_axis.x, ld.x0, "x_ordinate must be ten");
     assert.equal(ld.y_axis.y, ld.y0, "y_ordinate must be ten");
     assert.equal(ld.y_axis.dest_x, ld.x0, "dest_x must be a one thousand and ten");
     assert.equal(ld.y_axis.dest_y, ld.y0 - ld.height, "y_ordinate must be one");
 });
 
-QUnit.test("let fix the minimum of y_pas when height is greater than y_pas fixed", (assert) =>{
-    var prop = {
-        data : [12, 234, 17, 29],
-        label: ["un", "deux", "trois", "quatre"]
+QUnit.test("let fix the minimum of y_pas when height is greater than y_pas fixed and the data to be displayed is well known as string", (assert) =>{
+     var prop = {
+        age : [12, 234, 17, 29],
+        sexe: ["un", "deux", "trois", "quatre"],
+        nom: ['David', 'michel', 'Sarah', 'Paule'],
+        y: "nom"
     }
     var ld  = new Landmark(10, 9000, 1000, 5, prop);
     assert.ok(ld.height > ld.y_pas_min,"the height must be greater than the minimum of y_pas ");
-    assert.equal(ld.y_pas_min, 1, "the minimum of y_pas must be ten");
+    assert.equal(ld.y_pas_min, ld.height/prop[prop.y].length, "the minimum of y_pas must be ten");
 });
 
-QUnit.test("let fix the minimum of y_pas when height is smaller than y_pas_min fixed", (assert) =>{
+QUnit.test("let fix the minimum of y_pas when the data to be display is not known", (assert) =>{
     var prop = {
-        data : [12, 234, 17, 29],
-        label: ["un", "deux", "trois", "quatre"]
+        age : [12, 234, 17, 29],
+        sexe: ["un", "deux", "trois", "quatre"],
+        nom: ['David', 'michel', 'Sarah', 'Paule']
     }
     var ld  = new Landmark(10, 9000, 1000, 1, prop);
     assert.ok(ld.height <= 1,"the height must be greater than the minimum of y_pas ");
-    assert.equal(ld.y_pas_min, ld.height/2, "the minimum of y_pas must be one");
+    assert.equal(ld.y_pas_min, ld.height/prop.sexe.length, "the minimum of y_pas must be one");
 });
 
-QUnit.test("let fix the minimum of x_pas when the label is an array of string", (assert) =>{
+QUnit.test("let fix the minimum of x_pas when the data to be displayed is well known as string", (assert) =>{
     var prop = {
-        data : [12, 234, 17, 29],
-        label: ["un", "deux", "trois", "quatre"]
+        age : [12, 234, 17, 29],
+        sexe: ["un", "deux", "trois", "quatre"],
+        nom: ['David', 'michel', 'Sarah', 'Paule'],
+        x: "sexe"
     }
     var ld  = new Landmark(10, 9000, 1000, 1000, prop);
-    assert.equal(typeof prop.label[0], "string", "the type of label must be a string");
-    assert.equal(ld.x_pas_min, ld.width/prop.label.length, "the minimum of x_pas must be ");
+    assert.equal(typeof prop[prop.x][0], "string", "the type of label must be a string");
+    assert.equal(ld.x_pas_min, ld.width/prop[prop.x].length, "the minimum of x_pas must be 250");
 });
 
-QUnit.test("let fix the minimum of x_pas when width is greater than x_pas fixed", (assert) =>{
+QUnit.test("let fix the minimum of x_pas when the data to be displayed is not known", (assert) =>{
     var prop = {
-        data : [12, 234, 17, 29],
-        label: [11, 34, 69, 13]
+        age : [12, 234, 17, 29],
+        sexe: ["un", "deux", "trois", "quatre"],
+        nom: ['David', 'michel', 'Sarah', 'Paule']
+    }
+    var ld  = new Landmark(10, 9000, 1000, 1000, prop);
+    assert.equal(typeof prop.age[0], "number", "the type of label must be a number");
+    assert.equal(ld.x_pas_min, 1, "the minimum of x_pas must be 1");
+});
+
+QUnit.test("let fix the minimum of x_pas when width is greater than x_pas fixed and x is defined", (assert) =>{
+     var prop = {
+        age : [12, 234, 17, 29],
+        sexe: ["un", "deux", "trois", "quatre"],
+        nom: ['David', 'michel', 'Sarah', 'Paule'],
+        x: "age"
     }
     var ld  = new Landmark(10, 9000, 1000, 5, prop);
-    assert.equal(typeof prop.label[0], "number", "the type of label must be a number");
+    assert.equal(typeof prop[prop.x][0], "number", "the type of data must be a number");
     assert.ok(ld.width > 1,"the width must be greater than the minimum of x_pas (1)");
     assert.equal(ld.x_pas_min, 1, "the minimum of x_pas must be one");
 });
 
-QUnit.test("let fix the minimum of x_pas when width is smaller than x_pas_min fixed", (assert) =>{
-    var prop = {
-        data : [12, 234, 17, 29],
-        label: [1, 17, 69, 13]
-    }
-    var ld  = new Landmark(10, 10, 0.5, 1, prop);
-    assert.equal(typeof prop.label[0], "number", "the type of label must be a number");
-    assert.ok(ld.width <= 1,"the width must be greater than the minimum of x_pas ");
-    assert.equal(ld.x_pas_min, ld.width/2, "the minimum of x_pas must be ten");
-});*/
 
-QUnit.test("graduation of y_axis", (assert) =>{
-    var prop = {
-        data : [12, 234, 17, 29],
-        label: [1, 17, 69, 13]
-    }
-    var ld  = new Landmark(10, 10, 5, 13, prop);
-    var y_pas_t = [23, 24.625, 26.25, 27.875, 29.5, 31.125, 32.75, 34.375];
-    assert.equal(ld.y_pas.length, 8, "the number of label on y_axis");
-    y_pas_t.map((y_t, index) =>{
-        assert.equal(y_t, ld.y_pas[index]);
-    });
-});
-
-QUnit.test("graduation of x_axis when prop.label isn't a array of string", (assert) =>{
-    var prop = {
-        data : [12, 234, 17, 29],
-        label: [1, 17, 69, 13]
+QUnit.test("graduation of x_axis when data isn't a array of string", (assert) =>{
+     var prop = {
+        age : [12, 234, 17, 29],
+        sexe: ["un", "deux", "trois", "quatre"],
+        nom: ['David', 'michel', 'Sarah', 'Paule']
     }
     var ld  = new Landmark(10, 10, 5, 13, prop);
     var x_pas_t = [10, 11.25, 12.5, 13.75];
@@ -108,14 +106,132 @@ QUnit.test("graduation of x_axis when prop.label isn't a array of string", (asse
     }); 
 });
 
-QUnit.test("graduation of x_axis when prop.label is an array of number", (assert) =>{
+QUnit.test("graduation of x_axis when data is known as string", (assert) =>{
     var prop = {
-        data : [12, 234, 17, 29],
-        label: ["un", "deux", "trois", "quatre"]
+        age : [12, 234, 17, 29],
+        sexe: ["un", "deux", "trois", "quatre"],
+        nom: ['David', 'michel', 'Sarah', 'Paule'],
+        x: "nom"
     }
     var ld  = new Landmark(10, 10, 5, 13, prop);
-    var x_pas_t = []
+    var x_pas_t = [10, 11.25, 12.5, 13.75];
+    x_pas_t.map((x_t, index) =>{
+        assert.equal(x_t, ld.x_pas[index]);
+    });
 });
 
 
+QUnit.test("graduation of y_axis when y_data isn't defined ", (assert) =>{
+    var prop = {
+        age : [12, 234, 17, 29],
+        sexe: ["un", "deux", "trois", "quatre"],
+        nom: ['David', 'michel', 'Sarah', 'Paule'],
+    }
+    var ld  = new Landmark(10, 10, 5, 13, prop);
+    var y_pas_t = [23, 19.75, 16.5, 13.25];
+    assert.equal(ld.y_pas.length, 4, "the number of data on y_axis");
+    y_pas_t.map((y_t, index) =>{
+        assert.equal(y_t, ld.y_pas[index]);
+    });
+});
+
+QUnit.test("graduation of y_axis when y_data is known", (assert) =>{
+    var prop = {
+        age : [12, 234, 17, 29],
+        sexe: ["un", "deux", "trois", "quatre"],
+        nom: ['David', 'michel', 'Sarah', 'Paule'],
+        y: "nom"
+    }
+    var ld  = new Landmark(10, 10, 5, 13, prop);
+    var y_pas_t = [23, 19.75, 16.5, 13.25];
+    y_pas_t.map((y_t, index) =>{
+        assert.equal(y_t, ld.y_pas[index]);
+    });
+});
+
+QUnit.test("drawing x_axis with graduations", (assert) =>{
+    var prop = {
+        age : [12, 234, 17, 29],
+        sexe: ["un", "deux", "trois", "quatre"],
+        nom: ['David', 'michel', 'Sarah', 'Paule'],
+        x: "nom"
+    }
+    var ld  = new Landmark(10, 10, 5, 13, prop, true);
+    var x_pas_t = [10, 11.25, 12.5, 13.75];
+    assert.equal(ld.display_x_graduations, true, "display x_graduations must be true");
+    assert.equal(ld.x_axis.children.length, ld.x_pas.length, "the number of children of x_axis must be the number of graduations");
+});
+
+QUnit.test("checking the  x position (x and dest_x) of each x_graduations on x_axis", (assert) =>{
+    var prop = {
+        age : [12, 234, 17, 29],
+        sexe: ["un", "deux", "trois", "quatre"],
+        nom: ['David', 'michel', 'Sarah', 'Paule'],
+        x: "nom"
+    }
+    var ld  = new Landmark(10, 10, 5, 13, prop, true);
+    var x_pas_t = [10, 11.25, 12.5, 13.75];
+    ld.x_axis.children.map(({child}, index) =>{
+        assert.equal(child.x  + child.offsetX, ld.x_pas[index]);
+        assert.equal(child.dest_x + child.offsetX, ld.x_pas[index]);
+    });
+});
+
+QUnit.test("checking the  y position (y and dest_y) of each x_graduations on x_axis", (assert) =>{
+    var prop = {
+        age : [12, 234, 17, 29],
+        sexe: ["un", "deux", "trois", "quatre"],
+        nom: ['David', 'michel', 'Sarah', 'Paule'],
+        x: "nom"
+    }
+    var ld  = new Landmark(10, 10, 5, 13, prop, true);
+    var x_pas_t = [10, 11.25, 12.5, 13.75];
+    ld.x_axis.children.map(({child}, index) =>{
+        assert.equal(child.y, ld.y0 - 3);
+        assert.equal(child.dest_y, ld.y0 + 3);
+    });
+});
+
+QUnit.test("drawing y_axis with graduations", (assert) =>{
+    var prop = {
+        age : [12, 234, 17, 29],
+        sexe: ["un", "deux", "trois", "quatre"],
+        nom: ['David', 'michel', 'Sarah', 'Paule'],
+        x: "nom"
+    }
+    var ld  = new Landmark(10, 10, 5, 13, prop, false, true);
+    var y_pas_t = [23, 19.75, 16.5, 13.25];
+    assert.equal(ld.display_y_graduations, true, "display y_graduations must be true");
+    assert.equal(ld.y_axis.children.length, ld.y_pas.length, "the number of children of y_axis must be the number of graduations");
+});
+
+QUnit.test("checking the  x position (x and dest_x) of each y_graduations on y_axis", (assert) =>{
+    var prop = {
+        age : [12, 234, 17, 29],
+        sexe: ["un", "deux", "trois", "quatre"],
+        nom: ['David', 'michel', 'Sarah', 'Paule'],
+        x: "nom"
+    }
+    var ld  = new Landmark(10, 10, 5, 13, prop, false, true);
+    var y_pas_t = [23, 19.75, 16.5, 13.25];
+    ld.y_axis.children.map(({child}, index) =>{
+        assert.equal(child.x  + child.offsetX, ld.x0 - 3);
+        assert.equal(child.dest_x + child.offsetX, ld.x0 + 3);
+    });
+});
+
+QUnit.test("checking the  y position (y and dest_y) of each y_graduations on y_axis", (assert) =>{
+    var prop = {
+        age : [12, 234, 17, 29],
+        sexe: ["un", "deux", "trois", "quatre"],
+        nom: ['David', 'michel', 'Sarah', 'Paule'],
+        x: "nom"
+    }
+    var ld  = new Landmark(10, 10, 5, 13, prop, false, true);
+    var y_pas_t = [23, 19.75, 16.5, 13.25];
+    ld.y_axis.children.map(({child}, index) =>{
+        assert.equal(child.y, ld.y_pas[index]);
+        assert.equal(child.dest_y, ld.y_pas[index]);
+    });
+});
 
